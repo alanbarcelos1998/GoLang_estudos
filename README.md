2e6c2bd4ed3b732be582ab00e789253c6da29471
alanbarcelos1998 <alanbasiliobarcelos@hotmail.com>
2022-03-05 15:10:58 -0300

    testes no dir chapter8/math terminado

---
+++ README.md
@@ -0,0 +1,5 @@
+## Criar documentação do pacote
+    com o comando go doc golang-book/chapter8/math Average
+    - será exibido a documentação no terminal, dessa função.
+    é possível também mostrar em formato web, colocando o seguinte comando:
+        go doc -http=":6060"
---
+++ chapter8/math/math_test.go
@@ -0,0 +1,30 @@
+// o compilador do go ignora qualquer arquivo que tenha _test.go
+
+package math
+
+import "testing"
+
+type testpair struct {
+	values  []float64
+	average float64
+}
+
+var tests = []testpair{
+	{[]float64{1, 2}, 1.5},
+	{[]float64{1, 1, 1, 1, 1, 1}, 1},
+	{[]float64{-1, 1}, 0},
+}
+
+// Os testes são identificados pela palavra Test no início do nome de uma função que recebe como argumento *testing T
+func TestAverage(t *testing.T) {
+	for _, pair := range tests {
+		v := Average(pair.values)
+		if v != pair.average {
+			t.Error(
+				"For", pair.values,
+				"expected", pair.average,
+				"got", v,
+			)
+		}
+	}
+}
