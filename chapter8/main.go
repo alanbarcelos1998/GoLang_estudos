3a6b91f171b5897849deaa146cbbfad9a0a29ddc
alanbarcelos1998 <alanbasiliobarcelos@hotmail.com>
2022-03-05 11:48:59 -0300

    modulo de pacotes finalizado

--- chapter8/main.go
+++ chapter8/main.go
@@ -2,11 +2,18 @@ package main
 
 import (
 	"fmt"
-	"golang-book/chapter8/math"
+	// Podemos colocar apelidos no pacote que criamos, colocando na frente das aspas o apelido que queremos
+	m "golang-book/chapter8/math"
 )
 
 func main() {
 	xs := []float64{1, 2, 3, 4}
-	avg := math.Average(xs)
+	avg := m.Average(xs)
 	fmt.Println(avg)
+
+	min := m.Min(xs)
+	fmt.Println(min)
+
+	max := m.Max(xs)
+	fmt.Println(max)
 }
--- chapter8/math/math.go
+++ chapter8/math/math.go
@@ -1,5 +1,6 @@
 package math
 
+// calcula a média de uma série de números
 func Average(xs []float64) float64 {
 	total := float64(0)
 	for _, x := range xs {
@@ -7,4 +8,24 @@ func Average(xs []float64) float64 {
 	}
 
 	return total / float64(len(xs))
+}
+
+func Min(xs []float64) float64 {
+	min := 0.0
+	for i, v := range xs {
+		if i == 0 || v < min {
+			min = v
+		}
+	}
+	return min
+}
+
+func Max(xs []float64) float64 {
+	max := 0.0
+	for i, v := range xs {
+		if i == 0 || v > max {
+			max = v
+		}
+	}
+	return max
 }
