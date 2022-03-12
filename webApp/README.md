81f173be849e6a0eb743b86279ee8059090a86dd
alanbarcelos1998 <alanbasiliobarcelos@hotmail.com>
2022-03-12 11:44:48 -0300

    arquivos estaticos adicionados

---
+++ webApp/README.md
@@ -0,0 +1,3 @@
+## Redis
+    É um armazenamento de estrutura de dados em memória. 
+    É usado como banco de dados em cache e intermediário em mensagens.
--- webApp/main.go
+++ webApp/main.go
@@ -5,24 +5,50 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/go-redis/redis"
 	"github.com/gorilla/mux"
 )
 
+// global variables
 var templates *template.Template
+var client *redis.Client
 
 func main() {
+	client = redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+	})
+
 	templates = template.Must(template.ParseGlob("templates/*.html"))
 	r := mux.NewRouter()
 	r.HandleFunc("/contact", contactHandler).Methods("GET")
 	r.HandleFunc("/about", aboutHandler).Methods("GET")
-	r.HandleFunc("/", indexHandler).Methods("GET")
+	r.HandleFunc("/", indexGetHandler).Methods("GET")
+	r.HandleFunc("/", indexPostHandler).Methods("POST")
+	fs := http.FileServer(http.Dir("./static/"))
+	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", fs))
 	http.Handle("/", r)
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
 
 // request index page handle
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-	templates.ExecuteTemplate(w, "index.html", "This is the index Page")
+func indexGetHandler(w http.ResponseWriter, r *http.Request) {
+	comments, err := client.LRange(client.Context(), "comments", 0, 10).Result()
+
+	if err != nil {
+		return
+	}
+	templates.ExecuteTemplate(w, "index.html", comments)
+}
+
+// request index page POST handle
+func indexPostHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	// get the comment in html tag comment
+	comment := r.PostForm.Get("comment")
+	// push the comment to the comments list
+	client.LPush(client.Context(), "comments", comment)
+	// redirect tp / when the submit form
+	http.Redirect(w, r, "/", 302)
 }
 
 // request contact page handle
