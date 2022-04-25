a400c064382ded411e5bffb263f7045bf9a78fb3
alanbarcelos1998 <alanbasiliobarcelos@hotmail.com>
2022-04-25 16:05:54 -0300

    struct dir

---
+++ goUdemy/6-structs/struct.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+type usuario struct {
+	nome     string
+	idade    uint8
+	endereco endereco
+}
+
+type endereco struct {
+	logradouro string
+	numero     uint8
+}
+
+func main() {
+	var u usuario
+	u.nome, u.idade = "Alan", 23
+
+	fmt.Println(u)
+
+	usuario2 := usuario{
+		"Alan",
+		23,
+		endereco{
+			"Rua Domingos martins",
+			91,
+		},
+	}
+
+	fmt.Println(usuario2)
+}
