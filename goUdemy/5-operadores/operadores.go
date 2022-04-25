75b6dfcfe8c8e616227e9f84e52214195e253a0d
alanbarcelos1998 <alanbasiliobarcelos@hotmail.com>
2022-04-25 15:42:59 -0300

    operadores

---
+++ goUdemy/5-operadores/operadores.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	// ARITMÉTICOS
+	soma := 1 + 2
+	subtracao := 2 - 1
+	divisao := 4 / 2
+	multiplicacao := 2 * 2
+	restoDaDivisao := 10 % 2
+
+	fmt.Println(soma, subtracao, divisao, multiplicacao, restoDaDivisao)
+
+	var numero1 int16 = 10
+	var numero2 int16 = 25
+	var soma2 int16 = numero1 + numero2
+	fmt.Println(soma2)
+
+	// FIM DOS ARITMÉTICOS
+
+}
